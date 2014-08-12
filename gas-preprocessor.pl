72887b9b99669f9c3ef416667415af8009d4d38f
Janne Grunau <janne-libav@jannau.net>
2014-08-12 17:53:23 +0200

    allow a comma between macro name and first macro parameter
    Both gas and llvm's integrated assembler accept it.

--- gas-preprocessor.pl
+++ gas-preprocessor.pl
@@ -402,7 +402,7 @@ sub parse_line {
     } elsif ($macro_level == 0) {
         expand_macros($line);
     } else {
-        if ($line =~ /\.macro\s+([\d\w\.]+)\s*(.*)/) {
+        if ($line =~ /\.macro\s+([\d\w\.]+)\s*,?\s*(.*)/) {
             $current_macro = $1;
 
             # commas in the argument list are optional, so only use whitespace as the separator
